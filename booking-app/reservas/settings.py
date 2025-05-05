be20cd851112647ba528ebea42c5eee2e0bd4bc2
codigo-ba <152794362+codigo-ba@users.noreply.github.com>
2025-05-05 22:13:44 +0000

    Solving files distribution

--- booking-app/reservas/settings.py
+++ booking-app/reservas/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -56,7 +57,7 @@ ROOT_URLCONF = "reservas.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],  # 📌 Cambiado a booking-app/templates
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -117,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]  # 📌 Esto le dice a Django que busque en "reservas/static"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]  # 📌 Cambiado a booking-app/static
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
